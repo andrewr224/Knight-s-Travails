9994541e82bc9d1fbbfd33188bc5fd77cce5e07e
andrewr224 <andrew.r224@gmail.com>
2017-09-21 14:25:32 +0300

    Add. knight_moves method that finds the shortest path to the target

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,26 +1,13 @@
-class Board
-
-  def initialize
-    @squares = [[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]],[[],[],[],[],[],[],[],[]]]
-  end
-
-  def squares
-    @squares
-  end
-end
-
 class Knight
+  attr_reader :position
 
-  def initialize(col, row)
-    @position = [col, row]
-
+  def initialize(position=[0,0])
+    @position = position
   end
 
-  def position
-    @position
-  end
-
-  def move(col, row)
+  def move(target)
+    col = target[0]
+    row = target[1]
     return "Please provide valid input" unless ((col.is_a? Integer) && (col >= 0) && (col < 8))
     return "Please provide valid input" unless ((row.is_a? Integer) && (row >= 0) && (row < 8))
 
@@ -32,6 +19,58 @@ class Knight
       "Moved to a new square"
     else
       "Target is out of reach"
+    end
+  end
+
+  # calculates a path to the target sqaure
+  def self.knight_moves(start, finish)
+    start_point = Move.new(start)
+    queue = [start_point]
+    found = false
+    answer = []
+
+    until found
+      move = queue.shift
+      if move.position == finish
+        answer.unshift move.position
+        found = true
+        until move.parent.nil?
+          move = move.parent
+          answer.unshift move.position
+        end
+      end
+
+      move.possible_moves.each do |m|
+        queue << Move.new(m, move)
+      end
+    end
+    answer
+  end
+
+  class Move
+    attr_accessor :parent
+    attr_reader :position
+    def initialize(position, parent=nil)
+      @position = position
+      @parent = parent
+    end
+
+    def possible_moves
+      col = @position[0]
+      row = @position[1]
+
+      moves = []
+
+      moves << [col - 2, row - 1] if (0..7).include?(col - 2) && (0..7).include?(row - 1)
+      moves << [col - 2, row + 1] if (0..7).include?(col - 2) && (0..7).include?(row + 1)
+      moves << [col + 2, row - 1] if (0..7).include?(col + 2) && (0..7).include?(row - 1)
+      moves << [col + 2, row + 1] if (0..7).include?(col + 2) && (0..7).include?(row + 1)
+      moves << [col - 1, row - 2] if (0..7).include?(col - 1) && (0..7).include?(row - 2)
+      moves << [col - 1, row + 2] if (0..7).include?(col - 1) && (0..7).include?(row + 2)
+      moves << [col + 1, row - 2] if (0..7).include?(col + 1) && (0..7).include?(row - 2)
+      moves << [col + 1, row + 2] if (0..7).include?(col + 1) && (0..7).include?(row + 2)
+
+      moves
     end
   end
 end
--- spec/knight_specs.rb
+++ spec/knight_specs.rb
@@ -1,62 +1,7 @@
 require 'knight'
 
-# is a game
-#
-# has a board
-# board's dimentions are 8x8
-# board has 64 squares
-# board has 32 black squares
-# board has 32 white squares
-# a square can be empty
-# a square can be occupied
-#
-# has figures
-# has a knight
-# knight is a figure
-# knight has a position
-# a knight moves 1 square forward and 2 to the side, in any direction
-# can find a route
-# can find the shortest route
-
-
-
-RSpec.describe "Board" do
-  let(:game) { Board.new }
-
-  it "is a game" do
-    expect(game).to be_truthy
-  end
-
-  describe '#squares' do
-    it "has squares" do
-      expect(game.squares).to be_truthy
-    end
-
-    it "has 8 'rows'" do
-      expect(game.squares.length).to eq(8)
-    end
-
-    it "has 8 'columns'" do
-      expect(game.squares[0].length).to eq(8)
-    end
-
-    context "when square is empty" do
-      it { expect(game.squares[0][0]).to be_empty }
-    end
-
-    # not yet
-    #context "when square is not empty" do
-    #  let(:knight) { Knight.new(0,0) }
-    #  it "has a figure on it" do
-    #    expect(game.squares[0][0]).to eq(knight)
-    #  end
-    #end
-  end
-end
-
 RSpec.describe "Knight" do
-  let(:game) { Board.new }
-  let(:knight) { Knight.new(0,0) }
+  let(:knight) { Knight.new }
 
   it "has an initial position" do
     expect(knight.position).to_not be_nil
@@ -64,22 +9,38 @@ RSpec.describe "Knight" do
 
   describe '#move' do
     it "needs valid input" do
-      expect(knight.move(-1,'and')).to eq("Please provide valid input")
+      expect(knight.move([-1,'and'])).to eq("Please provide valid input")
     end
 
     it "can't move to a square that isn't within his range" do
-      expect(knight.move(7,7)).to eq("Target is out of reach")
+      expect(knight.move([7,7])).to eq("Target is out of reach")
     end
 
     it "can move to a square that is within his range" do
-      expect(knight.move(1,2)).to eq("Moved to a new square")
+      expect(knight.move([1,2])).to eq("Moved to a new square")
     end
 
     context "when moves to a new square" do
       it "has a position that of the square he moved to" do
-        knight.move(1,2)
+        knight.move([1,2])
         expect(knight.position).to eq([1,2])
       end
+    end
+  end
+
+  describe '.knight_moves' do
+    let(:path) { Knight.knight_moves([0,0],[6,7]) }
+    it "calculates the shortes way to the target" do
+      expect(Knight.knight_moves([0,0],[1,2])).to eq([[0,0],[1,2]])
+      expect(Knight.knight_moves([0,0],[3,3])).to eq([[0,0],[2,1],[3,3]])
+      expect(Knight.knight_moves([1,2],[6,7])).to_not be_empty
+    end
+
+    it "can then move to the target square [6,7]" do
+      path.each do |step|
+        knight.move(step)
+      end
+      expect(knight.position).to eq([6,7])
     end
   end
 end
